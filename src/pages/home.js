5f4abfe0eb91b3c56aa3c7d6386cc90e08e13526
Aaron Goldsmith <agoldsmith@alumni.scu.edu>
2018-10-24 11:10:41 -0700

    update header scrolling

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,44 @@
 import React, { Component } from 'react';
-import Card from "../components/PortfoCard/Card"
 import myself from "../agoldsmith.jpg"
 import Navtabs from "../components/Nav/Navtabs"
 import './pages.css';
 
 class Home extends Component
 {
+
+  state = {
+    scrolledDown:false,
+    page:'about'
+  }
+
+  handleScroll = () => {
+    if(window.scrollY>=260){
+      this.setState({scrolledDown:false})
+    }
+    else{
+      this.setState({scrolledDown:true})
+    }
+  }
+  handleChevron = () => {
+    if(window.scrollY>50){
+      window.scroll(0,-1)
+    }
+  }
+  
+  componentDidMount(){
+    this.setState({scrolledDown:true})
+    window.addEventListener('scroll', this.handleScroll);
+
+  }
+  componentWillUnmount(){
+    window.removeEventListener('scroll', this.handleScroll);
+  }
   render()
   {
     return (
       <div className="bg" id="app">
         <header className="App-header">
+          <i class="fas fa-envelope"></i>
           <div className="avatar">
             <img src={myself} alt='Aaron Goldsmith'></img>
           </div>
@@ -28,6 +56,8 @@ class Home extends Component
           </div>
           <Navtabs />
         </header>
+        {((!this.state.scrolledDown)?<i onClick={this.handleChevron} className="fas fa-chevron-up"></i>:<i></i>)}
+
       </div>
     );
   }
