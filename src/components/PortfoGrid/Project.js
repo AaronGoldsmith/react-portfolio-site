d36162a90f7b3d99fcce252c3c4d9ee6cb28a938
Aaron Goldsmith <agoldsmith@alumni.scu.edu>
2018-10-25 17:40:19 -0700

    removing portfolio-frame. restructuring page

--- src/components/PortfoGrid/Project.js
+++ src/components/PortfoGrid/Project.js
@@ -1,10 +1,9 @@
 import React from "react"
-
-const Project = (props) => (
-    <div>
-        <h3>{props.title}</h3>
-        <img src={props.imgSrc} alt={props.title} />
-        <p> Here is a little bit of text</p>
+import "./Project.css"
+const Project = ({project}) => (
+    <div className="project">
+        <h3>{project.pname}</h3>
+        <img src={project.imgSrc?project.imgSrc:''} alt={project.pname} />
     </div>
 );
 export default Project;
