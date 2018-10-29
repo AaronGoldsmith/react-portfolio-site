ae59184303d2b1beb81b2e531dd8b6d3fdea77d6
Aaron Goldsmith <agoldsmith@alumni.scu.edu>
2018-10-29 14:01:45 -0700

    formatting/remove comments

--- src/components/PortfoCard/CardContent.js
+++ src/components/PortfoCard/CardContent.js
@@ -1,7 +1,5 @@
 import React from "react";
 import "./Card.css"
-const regex = /[\[](.+)[\]][\(](.+)[\)]/;
-
 export const CardContent = ({children}) => (
   <div className="card-content">
     {children}
