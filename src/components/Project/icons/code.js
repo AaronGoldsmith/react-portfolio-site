7ae2fae263cac8b7d8bead9715883f84e3d06c9b
Aaron Goldsmith <agoldsmith@alumni.scu.edu>
2018-11-01 00:19:21 -0700

    fixed spacing with items under thumbnails

--- src/components/Project/icons/code.js
+++ src/components/Project/icons/code.js
@@ -2,10 +2,10 @@ import React from "react"
 const SVG =({
   width = "100%",
   height = "100%",
-  viewBox = "-50 -90 250.00001 450.00001"
+  viewBox = "90 90 360 450"
 }) => (
 <svg className='svg' width={width} height={height} version="1.1" viewBox={viewBox} xmlns="http://www.w3.org/2000/svg">
- <g transform="rotate(35) translate(-100 -652.36)" fill="none" stroke="#555" strokeLinecap="round" strokeLinejoin="round" strokeWidth="38.966">
+ <g transform="rotate(25) translate(120 -552.36)" fill="none" stroke="#555" strokeLinecap="round" strokeLinejoin="round" strokeWidth="38.966">
   <path d="m125.28 600.91-101.56 101.56 101.56 101.56"/>
   <path d="m376.97 600.91 101.56 101.56-101.56 101.56"/>
   <path d="m274.86 570.23-54.621 265.77"/>
